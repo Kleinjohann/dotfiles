69df33dad21f753af8c57f797683dd3b40246338
kleinjohann <a.kleinjohann@fz-juelich.de>
2020-05-29 20:56:18 +0200

    Add temperature, cpu and ram per process on click

---
+++ config/polybar.xsymlink/scripts/cpu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+case "$1" in
+	--popup)
+		notify-send "CPU time (%)" "$(ps axch -o cmd,pcpu k -pcpu | head | awk '$NF=$NF"%"' )"
+		;;
+	*)
+	# Read /proc/stat file (for first datapoint)
+	read cpu user nice system idle iowait irq softirq steal guest< /proc/stat
+
+	# compute active and total utilizations
+	cpu_active_prev=$((user+system+nice+softirq+steal))
+	cpu_total_prev=$((user+system+nice+softirq+steal+idle+iowait))
+
+	sleep .05
+
+	# Read /proc/stat file (for second datapoint)
+	read cpu user nice system idle iowait irq softirq steal guest< /proc/stat
+
+	# compute active and total utilizations
+	cpu_active_cur=$((user+system+nice+softirq+steal))
+	cpu_total_cur=$((user+system+nice+softirq+steal+idle+iowait))
+
+	# compute CPU utilization (%)
+	cpu_util=$((100*( cpu_active_cur-cpu_active_prev ) / (cpu_total_cur-cpu_total_prev) ))
+
+	printf "%s%%" "$cpu_util"
+
+esac
+
---
+++ config/polybar.xsymlink/scripts/memory.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+case "$1" in
+    --popup)
+        notify-send "Memory (MB)" "$(ps axch -o cmd,rss k -rss | head | awk '$NF=int($NF/1024)"M"' )"
+        ;;
+    *)
+        echo "$(free -h --si | awk '/^Mem:/ {print $3 "/" $2}')"
+        ;;
+esac
