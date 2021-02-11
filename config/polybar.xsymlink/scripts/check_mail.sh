05af74bc41d52d6dcda395ce0359f8e4c0530a31
kleinjohann <a.kleinjohann@fz-juelich.de>
2021-02-11 12:36:32 +0100

    Clarify comments and code in check_mail.sh

--- config/polybar.xsymlink/scripts/check_mail.sh
+++ config/polybar.xsymlink/scripts/check_mail.sh
@@ -6,10 +6,10 @@ ICON=" "
 
 # We need the first occurrence of `^A2=[0-9]` within the last set of [] that contains this pattern
 # 1. get all [] containing `^A2=[0-9]`
-# 2. only keep tha last set of []
-# 3. remove newlines to search the entire []
-# 4. find the first occurrence of `^A2=[0-9]` and extract the number
-COUNT=$(sed -n '/.*\[/{:start /\]/!{N;b start};/.*^A2.*/p}' "$(find ~/.thunderbird -name 'INBOX.msf' )" | sed -zn 's/.*\[\(.*\)/\1/p' | tr -s '\n' ' ' | perl -pe "s|.*?A2=([0-9]*).*|\1|p")
+# 2. only keep the content of the last []
+# 3. remove newlines to replace the entire string in the next step
+# 4. find the first occurrence of `^A2=[0-9]` and only return the number
+COUNT=$(sed -n '/.*\[/{:start /\]/!{N;b start};/.*^A2=[0-9]*.*/p}' "$(find ~/.thunderbird -name 'INBOX.msf' )" | sed -zn 's/.*\[\(.*\)/\1/p' | tr -s '\n' ' ' | perl -pe "s|.*?\^A2=([0-9]*).*|\1|p")
 
 # Display the formatted message
 if (( COUNT > 0)); then
